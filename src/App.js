56f2117c8bf1c8b2579971b780f75b6e6cc164ed
islahola <islah.khofifah@gmail.com>
2021-12-05 00:02:41 +0700

    masi test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>islah olla test coba yas, coba lgi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
